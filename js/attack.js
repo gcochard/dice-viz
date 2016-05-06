e7931499e272302cd88b3acf260afea7a91e4b4b
John Gill <king2bgill@gmail.com>
2016-05-06 10:47:52 -0700

    Correct colorScale domain

--- js/attack.js
+++ js/attack.js
@@ -134,8 +134,7 @@ function vizAttackData(redrawSlider) {
     var colors = ['#a50026','#d73027','#f46d43','#fdae61','#fee08b','#ffffbf','#d9ef8b','#a6d96a','#66bd63','#1a9850','#006837'].reverse()
     var colorScale = d3.scale.quantile()
         .domain([d3.min(matrix, function (d) { return d.weight; }),
-            colors.length - 1,
-            d3.max(matrix, function (d) { return d.weight; })])
+                 d3.max(matrix, function (d) { return d.weight; })])
         .range(colors);
     var svg = d3.select("svg");
     svg.call(tip);
